d5025dcf6df42595d179b1e6c559c35db553ae6a
Patrick Story <ptedstoryv2@gmail.com>
2021-09-18 23:17:53 -0400

    Update modular_test.ino

--- MainSrc/modular_test.ino
+++ MainSrc/modular_test.ino
@@ -23,11 +23,8 @@ const uint8_t PWR_MGMT_1 = 0x6B;
 const uint8_t ACCEL_CONFIG = 0x1c;
 enum ACCEL_REGISTERS {ACCEL_XOUT_H=0x3B, ACCEL_XOUT_L, ACCEL_YOUT_H, ACCEL_YOUT_L, ACCEL_ZOUT_H, ACCEL_ZOUT_L};
 /* MPU6050 Variables */
-
-
 const uint8_t config_map = 0b00001000;
 float LSB_per_g = 8192;
-float error;
 
 /*RF24 variables */
 const byte addr[6] = "Recv1";
@@ -70,44 +67,10 @@ int read_accel_data(uint8_t mpu_addr, bool end_trans, float lsb_per_g, struct ac
 }
 
 
-float calibrate_accelerometer(uint8_t mpu_addr, bool instruct, float lsb_per_g, uint32_t limit){
-  struct accel_coordinates xyz;
-  float error = 1.0;
-  float d_step = 0.001;
-  
-  read_accel_data(mpu_addr, true, lsb_per_g, &xyz);
-  
-  if(instruct){
-    Serial.println("Place MPU6050 on a flat surface and wait for the error to be found");  
-  }
-  while((xyz.x != 0 && xyz.y != 0) && limit > 0){
-      if(xyz.x > 0 or xyz.y > 0){
-        error+=d_step; 
-       } else if(xyz.x < 0 or xyz.y < 0){ 
-        error-=d_step;
-       }
-      read_accel_data(mpu_addr, true, lsb_per_g, &xyz);
-      xyz.x/=error;
-      xyz.y/=error;
-      if(xyz.x== 0 or xyz.y == 0){
-        limit = 0;  
-      } else {
-        limit-=1;
-        }
-  }
-
-  return error;
-}
-
 void setup() {
   Serial.begin(9600);
   /* MPU6050 setup */
-  Serial.println("Setting up accelerometer");
   while(setup_accelerometer(MPUADDR, PWR_MGMT_1, ACCEL_CONFIG, config_map, true) != 0);
-  Serial.println("Calibrating accelerometer");
-  error = calibrate_accelerometer(MPUADDR, false, LSB_per_g, 0xFFFFFFF);
-  Serial.println("Finished calibrating, error found: ");
-  Serial.print(error);
   /* RF24 setup */
   if(!RF24Chip.begin()){
   }
@@ -122,9 +85,6 @@ void loop() {
   int x = 1;
   struct accel_coordinates xyz; 
   read_accel_data(MPUADDR, false, LSB_per_g, &xyz);
-//  Serial.print("X: ");
-//  Serial.print("Y: ");
-  Serial.println(xyz.y);
   
  
  if(RF24Chip.available(pipe)){
