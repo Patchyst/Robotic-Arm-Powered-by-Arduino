f0aa24a578a56c429f2c17e7b613286f2ae0c1ef
Patrick Story <ptedstoryv2@gmail.com>
2021-09-18 23:07:33 -0400

    Update modular_test.ino

--- MainSrc/modular_test.ino
+++ MainSrc/modular_test.ino
@@ -25,7 +25,8 @@ enum ACCEL_REGISTERS {ACCEL_XOUT_H=0x3B, ACCEL_XOUT_L, ACCEL_YOUT_H, ACCEL_YOUT_
 /* MPU6050 Variables */
 
 
-const uint16_t config_map = 0b0000000000001000;
+const uint8_t config_map = 0b00001000;
+float LSB_per_g = 8192;
 float error;
 
 /*RF24 variables */
@@ -34,34 +35,34 @@ const int pipe = 0;
 
 RF24 RF24Chip(CEPIN, CSPIN, RF24_SPI_SPEED);
 
-int setup_accelerometer(uint8_t mpu_addr, uint8_t pwr_mgmt, uint8_t accel_config, uint16_t config_bitmap, bool startI2C){
+int setup_accelerometer(uint8_t mpu_addr, uint8_t pwr_mgmt, uint8_t accel_config, uint8_t config_bitmap, bool startI2C){
   if(startI2C){
     Wire.begin();
   }
   
   Wire.beginTransmission(mpu_addr);
   Wire.write(pwr_mgmt);
-  Wire.write((uint8_t)(config_bitmap & 0xff00));
+  Wire.write(0x00);
   while(Wire.endTransmission(true) != 0x00);
 
   Wire.beginTransmission(mpu_addr);
   Wire.write(accel_config);
-  Wire.write((uint8_t)((config_bitmap & 0x00ff)<<8));
+  Wire.write(config_bitmap);
   while(Wire.endTransmission(true) != 0x00);
 
   return 0;
 }
 
-int read_accel_data(uint8_t mpu_addr, bool end_trans, struct accel_coordinates *raw_xyz){
+int read_accel_data(uint8_t mpu_addr, bool end_trans, float lsb_per_g, struct accel_coordinates *raw_xyz){
 
  Wire.beginTransmission(mpu_addr);
  Wire.write(0x3B);
  Wire.endTransmission(false);
  Wire.requestFrom(mpu_addr, 6, true);
   
-  raw_xyz->x = Wire.read() << 8 | Wire.read();
-  raw_xyz->y = Wire.read() << 8 | Wire.read();
-  raw_xyz->z = Wire.read() << 8 | Wire.read();
+  raw_xyz->x = (Wire.read() << 8 | Wire.read()) / lsb_per_g;
+  raw_xyz->y = (Wire.read() << 8 | Wire.read()) / lsb_per_g;
+  raw_xyz->z = (Wire.read() << 8 | Wire.read()) / lsb_per_g;
   if(end_trans){
     Wire.endTransmission(true);
    }
@@ -69,12 +70,12 @@ int read_accel_data(uint8_t mpu_addr, bool end_trans, struct accel_coordinates *
 }
 
 
-float calibrate_accelerometer(uint8_t mpu_addr, bool instruct, uint32_t limit){
+float calibrate_accelerometer(uint8_t mpu_addr, bool instruct, float lsb_per_g, uint32_t limit){
   struct accel_coordinates xyz;
   float error = 1.0;
   float d_step = 0.001;
   
-  read_accel_data(mpu_addr, true, &xyz);
+  read_accel_data(mpu_addr, true, lsb_per_g, &xyz);
   
   if(instruct){
     Serial.println("Place MPU6050 on a flat surface and wait for the error to be found");  
@@ -85,7 +86,7 @@ float calibrate_accelerometer(uint8_t mpu_addr, bool instruct, uint32_t limit){
        } else if(xyz.x < 0 or xyz.y < 0){ 
         error-=d_step;
        }
-      read_accel_data(mpu_addr, true, &xyz);
+      read_accel_data(mpu_addr, true, lsb_per_g, &xyz);
       xyz.x/=error;
       xyz.y/=error;
       if(xyz.x== 0 or xyz.y == 0){
@@ -104,7 +105,7 @@ void setup() {
   Serial.println("Setting up accelerometer");
   while(setup_accelerometer(MPUADDR, PWR_MGMT_1, ACCEL_CONFIG, config_map, true) != 0);
   Serial.println("Calibrating accelerometer");
-  error = calibrate_accelerometer(MPUADDR, false, 0xFFFFFFF);
+  error = calibrate_accelerometer(MPUADDR, false, LSB_per_g, 0xFFFFFFF);
   Serial.println("Finished calibrating, error found: ");
   Serial.print(error);
   /* RF24 setup */
@@ -118,12 +119,12 @@ void setup() {
 }
 
 void loop() {
+  int x = 1;
   struct accel_coordinates xyz; 
-  read_accel_data(MPUADDR, false, &xyz);
+  read_accel_data(MPUADDR, false, LSB_per_g, &xyz);
 //  Serial.print("X: ");
-  Serial.println(xyz.x);
 //  Serial.print("Y: ");
-//  Serial.println(xyz.y);
+  Serial.println(xyz.y);
   
  
  if(RF24Chip.available(pipe)){
